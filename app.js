15063674ced917c385752f1ba65d3f8ad37e61d8
ahmed <ahmedtalha.miah@gmail.com>
2023-06-16 06:25:43 +0100

    initialized git repository

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ for (let i = 1; i < 20; i++) {
     if (i % 3 === 0) {
         console.log(`${i} -> frontend`)
     }
+    if (i % 5 === 0) {
+        console.log(`${i} -> frontend`)
+    }
     if (i % 3 === 0) {
         console.log(`${i} -> frontend`)
     }
