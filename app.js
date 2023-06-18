09b104f10305720eae50f0267ad6e366c017884d
ahmed <ahmedtalha.miah@gmail.com>
2023-06-18 11:15:27 +0100

    initialized git repository

--- app.js
+++ app.js
@@ -1,14 +1,8 @@
-for (let i = 1; i < 20; ++i) {
-    if (i % 3 === 0 && i % 5 === 0) {
-        console.log(`${i} -> Frontend simplified`)
-    }
-    else if (i % 3 === 0) {
-        console.log(`${i} -> Frontend`)
-    }
-    else if (i % 5 === 0) {
-        console.log(`${i} -> Simplified`)
-    }
-    else {
-        console.log(`${i} -> ${i}`)
-    }
+let dollars = [1, 5, 10, 3]
+
+let cents = [];
+
+for (let i = 0; i < dollars.length; ++i) {
+    console.log(dollars[i]);
 }
+
