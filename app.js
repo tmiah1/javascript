183b60ef670ad9c267f0292a4c7850cc8f238902
ahmed <ahmedtalha.miah@gmail.com>
2023-06-25 16:47:33 +0100

    initialized git repository

--- app.js
+++ app.js
@@ -1,3 +1,22 @@
-function toggleDarkmode() {
-    document.querySelector('body').classlist.toggle("Dark-theme");
+
+const statusRef = document.querySelector(".status")
+
+
+
+
+function getVideo(subscriptionStatus) {
+return new Promise ((resolve, reject) => {
+    if(subscriptionStatus === "VIP"){
+        resolve("showVideo")
+    }
+    else if (subscriptionStatus === "FREE") {
+        resolve("show trailer")
+    }
+    else {
+        reject("no video")
+    }
+
+    })
 }
+
+console.log(getSubscriptionStatus())
