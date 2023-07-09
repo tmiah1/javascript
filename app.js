8b7f968da39791492850d5e52d67e4a61249f374
ahmed <ahmedtalha.miah@gmail.com>
2023-07-09 18:19:39 +0100

    initialized git repository

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
-function calcRemainder(num1, num2) { 
-return num1 % num2;
+function convertToZeros(arr) {
+    let sum = 0;
+    for (let i = 0; i < arr.length; ++i){
+        sum = sum + arr[i];
+    }
+
+    return sum;
 }
 
-console.log(calcRemainder(4, 6));
+console.log(convertToZeros([1, 2, 3, 4, 5]));
